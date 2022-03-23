d0c4cff05d45461b90934e1776e85d068922d497
Frederick Ziola <78122421+RickyZiola@users.noreply.github.com>
2022-03-23 18:26:50 +0000

    Made it function based rather than 1 pass

--- Main.java
+++ Main.java
@@ -5,16 +5,23 @@ import java.awt.*;
 import com.diogonunes.jcolor.*;
 
 class Main {
-  public static void main(String[] args) throws Throwable {
-    BufferedImage img = ImageIO.read(new File("image.jpg"));
-    for(int y = 0; y < img.getHeight(); y+= 10) {
-      for(int x = 0; x < img.getWidth(); x+=5) {
+  static void drawImage(BufferedImage img) {
+    for (int y = 0; y < img.getHeight(); y += 10) {
+      for (int x = 0; x < img.getWidth(); x += 5) {
         Color col = new Color(img.getRGB(x, y));
         Color col2 = new Color(img.getRGB(x, y + 1));
-  
-        System.out.print(Ansi.colorize("▄", Attribute.BACK_COLOR(col.getRed(), col.getGreen(), col.getBlue()), Attribute.TEXT_COLOR(col2.getRed(), col2.getGreen(), col2.getBlue())));
+
+        drawPixel(col, col2);
       }
       System.out.println();
     }
+  }
+  static void drawPixel (Color col, Color col2) {
+    System.out.print(Ansi.colorize("▄", Attribute.BACK_COLOR(col.getRed(), col.getGreen(), col.getBlue()),
+            Attribute.TEXT_COLOR(col2.getRed(), col2.getGreen(), col2.getBlue())));
+  }
+  public static void main(String[] args) throws Throwable {
+    BufferedImage img = ImageIO.read(new File("image.jpg"));
+    drawImage(img);
   }
 }
